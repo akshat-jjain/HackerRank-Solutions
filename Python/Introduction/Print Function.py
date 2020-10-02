bdc3c28d9c7376b64e52610b816c816fa7ff3513
AKSHAT JAIN <56213740+akshat-jjain@users.noreply.github.com>
2020-10-02 18:26:02 +0530

    Create Print Function.py

---
+++ Python/Introduction/Print Function.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    a = []
+    d = ""
+    n = int(input())
+    for x in range(1,n+1) :
+        a.append(x)
+    for b in a :
+        c = str(b)
+        d += c
+    print(d)
+
+# Code credit :- Akshat Jain  
+# HackerRank Link :- https://www.hackerrank.com/akshat_jjain?hr_r=1
