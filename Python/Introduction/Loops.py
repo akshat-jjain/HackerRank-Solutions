a2b3365915c9013334e738ee2f9d5dfffdf06172
AKSHAT JAIN <56213740+akshat-jjain@users.noreply.github.com>
2020-10-02 18:24:30 +0530

    Create Loops.py

---
+++ Python/Introduction/Loops.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n = int(input())
+    for x in range(0,n) :
+        print("{}".format(x*x))
+
+# Code credit :- Akshat Jain  
+# HackerRank Link :- https://www.hackerrank.com/akshat_jjain?hr_r=1
