751f509cd99e535bf589590de0a178f2e0ce071a
AKSHAT JAIN <56213740+akshat-jjain@users.noreply.github.com>
2020-10-02 18:23:31 +0530

    Create Python: Division.py

---
+++ Python/Introduction/Python: Division.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(int(a/b))
+    print(float(a/b))
+
+# Code credit :- Akshat Jain  
+# HackerRank Link :- https://www.hackerrank.com/akshat_jjain?hr_r=1
