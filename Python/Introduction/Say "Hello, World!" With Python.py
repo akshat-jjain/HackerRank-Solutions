b11c6a58f355c644b74d10259bc58274e09b3a08
AKSHAT JAIN <56213740+akshat-jjain@users.noreply.github.com>
2020-10-02 18:20:29 +0530

    Update Say "Hello, World!" With Python.py

--- Python/Introduction/Say "Hello, World!" With Python.py
+++ Python/Introduction/Say "Hello, World!" With Python.py
@@ -1,4 +1,4 @@
 print("Hello, World!")
 
-// Code credit :- Akshat Jain  
-//HackerRank Link :- https://www.hackerrank.com/akshat_jjain?hr_r=1
+# Code credit :- Akshat Jain  
+# HackerRank Link :- https://www.hackerrank.com/akshat_jjain?hr_r=1
