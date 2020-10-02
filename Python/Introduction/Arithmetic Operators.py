9e6772efdcbe7b726263e53be78b3ccfa00f3f00
AKSHAT JAIN <56213740+akshat-jjain@users.noreply.github.com>
2020-10-02 18:22:28 +0530

    Create Arithmetic Operators.py

---
+++ Python/Introduction/Arithmetic Operators.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+
+# Code credit :- Akshat Jain  
+# HackerRank Link :- https://www.hackerrank.com/akshat_jjain?hr_r=1
