d0485cdb16c73614e9c75e3f50df25e844dbc3b2
AKSHAT JAIN <56213740+akshat-jjain@users.noreply.github.com>
2020-10-03 12:44:35 +0530

    Update Tuples.py

--- Python/Basic Data Types/Tuples.py
+++ Python/Basic Data Types/Tuples.py
@@ -1,7 +1,8 @@
-if __name__ == '__main__':
+# Please make sure to check Indentation if code shows error
     n = int(input())
     integer_list = map(int, input().split())
     t = tuple(integer_list)
     print(hash(t))
 
-# Code credit :- Akshat Jain  # HackerRank Link :- https://www.hackerrank.com/akshat_jjain?hr_r=1
+# Code credit :- Akshat Jain  
+# HackerRank Link :- https://www.hackerrank.com/akshat_jjain?hr_r=1
