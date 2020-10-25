fd10ee0b79c153ac6dc8627d8b750be6451d2726
AKSHAT JAIN <56213740+akshat-jjain@users.noreply.github.com>
2020-10-25 12:57:08 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>HackerRank Solutions</title>
+	<style>
+		.domain-name {display:inline-block;width:37%;border:1px solid transparent;border-radius:5px;background-color:gold;padding:5%;text-align:center;margin:1%;}.domain-name:hover{box-shadow:1px 1px 1px 2px grey;}
+	</style>
+</head>
+<body>
+		<a href="./tree/master/C"><div class="domain-name">C</div></a>
+		<div class="domain-name">C++</div>
+        <div class="domain-name">Java</div>
+		<a href="./tree/master/Python"><div class="domain-name">Python</div></a>
+        <div class="domain-name">Sql</div>
+		<div class="domain-name">Problem Solving</div>
+        <div class="domain-name">30 Days of Code</div>
+		<div class="domain-name">10 Days of JS</div>
+</body>
+</html>
